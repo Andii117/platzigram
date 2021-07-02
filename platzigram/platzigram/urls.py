b614699d718c2d6860019c65a1720fc60ade076f
Andii117 <andresjara640@gmail.com>
2021-07-02 18:51:51 -0500

    Actualizando y mostrando vista con diccionario de datos con imagenes desde la apps posts

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -1,12 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from platzigram import views
+from platzigram import views as local_views
+from posts import views as posts_views 
 
 
 
 urlpatterns = [
-    path('hello-word/', views.hello_word ),
-    path('sorted/', views.hi ),
-    path('hi/<str:name>/<int:age>', views.say_hi)
+    path('hello-word/', local_views.hello_word ),
+    path('sorted/', local_views.hi ),
+    path('hi/<str:name>/<int:age>', local_views.say_hi),
+    path('posts/', posts_views.list_posts)
 ]
