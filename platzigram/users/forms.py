c6a866011ce05861cfaaff79762bd0ef6ea2d1a6
Andii117 <andresjara640@gmail.com>
2021-07-11 12:06:22 -0500

    actualización de la vista de signup para que funcione con el formulario, corrección del modelo para cambiar el nombre de a firts_name

--- platzigram/users/forms.py
+++ platzigram/users/forms.py
@@ -2,6 +2,9 @@
 
 #Django
 from django import forms
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
 
 class ProfileForm(forms.Form):
     #Profile forms
@@ -10,3 +13,67 @@ class ProfileForm(forms.Form):
     biography = forms.CharField(max_length=500, required=False)
     phone_number = forms.CharField(max_length=20, required=False)
     picture = forms.ImageField()
+
+class  SignupForm(forms.Form):
+    #Sign up form
+
+    username = forms.CharField(min_length=4, max_length=50)
+    password = forms.CharField(max_length=70, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(max_length=70, widget=forms.PasswordInput())
+
+    first_name = forms.CharField(min_length=2, max_length=50)
+    last_name  = forms.CharField(min_length=2, max_length=50)
+
+    email = forms.CharField(min_length= 6 , max_length= 70, widget= forms.EmailInput)
+
+
+    
+    def clean_username(self):
+        #Username must be unique.
+
+        #Obtenemos el dato de username validado
+        username = self.cleaned_data['username']
+        #consultamos a bd si existe
+        #filtre los usuarios en BD con username = username y obtenga todos los datos
+        username_taken = User.objects.filter(username = username).exists()
+        #Si existe el usuario         
+        if username_taken :
+            #Django se encarga de lanzar el error
+            raise forms.ValidationError('Username  is already in use.')
+        #En caso de que no este en uso, retorna el username
+        return username
+
+    def save(self):
+        #Create user and profile
+
+        #Obtenemos la información del formulario
+        data = self.cleaned_data
+        #Se saca de la lista el dato password_confirmation
+        data.pop('password_confirmation')
+
+        #import pdb; pdb.set_trace()
+        #Se crea el usuario en BD
+        user = User.objects.create_user(**data)
+        #Se crea el profile en BD
+        profile = Profile(user=user)
+        #Se guarda en BD
+        profile.save()
+
+
+
+    def clean(self):
+        #Verify password confirmation match.
+
+        #Obtenemos la información
+        data = super().clean()
+
+        #Obtenemos el password
+        password = data['password']
+        #Obtenemos el password confirmation
+        password_confirmation = data['password_confirmation']
+        #Validamos que sean iguales
+        if password != password_confirmation:
+            raise forms.ValidationError('Password do not match.')
+        
+        return data
+             
--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -3,12 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Exception
-from django.db.utils import IntegrityError
-
-# Models
-from django.contrib.auth.models import User
-from users.models import Profile
+#Forms
+from users.forms import ProfileForm, SignupForm
 
 # Forms
 from users.forms import ProfileForm
@@ -75,38 +71,20 @@ def logout_view(request):
 def singup_view(request):
     #Signup a user
     if request.method == 'POST':
-        #Obtiene el username, password y password confirma
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirm = request.POST['password_confirmation']
-        #Valida que la contraseña y su confirmación coincidan
-        if password != password_confirm: 
-            #Si no coinciden muestra el error en la pantalla de signup
-            return render(request, 'users/signup.html', {'error': 'password confirmation does not match'})
-        #Intenta crear el usuario de la BD
-        try:
-            #Usuario guardado en DB
-            user =  User.objects.create_user(username= username, password= password)
-        #Si el usuario ya esta creado
-        except IntegrityError:
-            #Si el usuario a crear ya se encuentra registrado en la DB muestra el mensaje de error
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+        form = SignupForm(request.POST)
 
-        #Obtiene los datos de first name last name y email del formulario
-        user.first_name = request.POST['first_name'] 
-        user.last_name  = request.POST['last_name'] 
-        user.email      = request.POST['email'] 
-        #Guarda la información del usuario
-        user.save()
-
-        #se crea el profile 
-        profile = Profile(user=user)
-        #Guarda la información del perfil
-        profile.save()
-        #Renderea al login
-        return redirect('login')
-
-    #renderea la pagina signup de la carpeta templates
-    return render(request, 'users/signup.html')
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={
+            'form' : form
+        }
+    )
 
 
+        
